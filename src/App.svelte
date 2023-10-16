db57ebf9406f346a499fe4d34b7dc9307c364448
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-10-16 18:22:12 +0530

    Code Updated

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,31 @@
 	import { Chart, ColumnSeries, LineSeries, Category, Legend, Tooltip, SplineSeries } from '@syncfusion/ej2-charts';
 	import { Browser } from '@syncfusion/ej2-base';
 	Chart.Inject(ColumnSeries, LineSeries, Category, Legend, Tooltip, SplineSeries);
+	import Card from './Card.svelte';
   
+	const API_BASE_URL = 'https://api.recruitly.io/api/dashboard/sales/data';
+    const API_KEY = 'TEST45684CB2A93F41FC40869DC739BD4D126D77';
+
+  let data = null;
+
+  onMount(async () => {
+	try {
+	  const response = await fetch(
+		'https://api.recruitly.io/api/dashboard/sales/metrics?start=01%2F01%2F2023&end=11%2F10%2F2023&apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77',
+		{
+		  headers: {
+			'Cookie': 'SESSION=NTkwN2VlOWQtZjRlNi00NmQ4LWE4MTUtOTJhNT71YjA0ZWMx',
+		  },
+		}
+	  );
+	  data = await response.json();
+	} catch (error) {
+	  console.error('Error fetching data:', error);
+	}
+  });
 	onMount(async () => {
 	  // Code for the "Deal Lifecycle Days" chart
-	  const apiUrlDays = 'https://api.recruitly.io/api/dashboard/sales/data/opportunitymonthlymetrics?start=01%2F01%2F2023&end=11%2F10%2F2023&apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77';
+	  const apiUrlDays = `${API_BASE_URL}/opportunitymonthlymetrics?start=01%2F01%2F2023&end=11%2F10%2F2023&apiKey=${API_KEY}`;
 	  const responseDays = await fetch(apiUrlDays);
 	  const dataDays = await responseDays.json();
 	  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -74,7 +95,7 @@
 	  chartDays.appendTo('#chart-container-days');
   
 	  // Code for the "Opportunity Value by User" chart
-	  const apiUrlOpportunity = 'https://api.recruitly.io/api/dashboard/sales/data/opportunitymonthlyusermetrics?start=01%2F10%2F2022&end=01%2F10%2F2023&apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77';
+	  const apiUrlOpportunity = `${API_BASE_URL}/opportunitymonthlyusermetrics?start=01%2F10%2F2022&end=01%2F10%2F2023&apiKey=${API_KEY}`;
 	  const responseOpportunity = await fetch(apiUrlOpportunity);
 	  const dataOpportunity = await responseOpportunity.json();
   
@@ -130,9 +151,46 @@
 	  padding: 16px;
 	  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 	}
+
+	main {
+	  display: flex;
+	  flex-wrap: wrap;
+	  justify-content: center;
+	}
+  
+	.card {
+	  width: 200px; /* Set the width to the desired size */
+	  height: 200px; /* Set the height to the desired size */
+	  margin: 10px; /* Add some margin between cards if needed */
+	}
   </style>
   
   <body>
+	<main>
+	{#if data}
+	  <div class="card">
+		<Card title="Leads Converted" value={data.leadsConverted} icon="fal fa-lg fa-check-square" />
+	  </div>
+	  <div class="card">
+		<Card title="Opportunities" value={data.opportunityCountOpen} icon="fal fa-lg fa-fire" />
+	  </div>
+	  <div class="card">
+		<Card title="Won from {data.opportunityCountWon} deals" value=" GBP {data.opportunityValueWon}"icon="fas fa-dollar-sign" />
+	  </div>
+	  <div class="card">
+		<Card title="Avg. Deal Cycle" value={data.avgDealCycleDays} icon="fal fa-lg fa-stopwatch" />
+	  </div>
+	  <div class="card">
+		<Card title="Avg.Deal Value" value="GBP{data.avgDealValue}" icon="fas fa-dollar-sign"/>
+	  </div>
+	  <div class="card">
+		<Card title="Leads/Opportunity" value={data.leadsPerOpportunity} icon="fal fa-lg fa-crosshairs" />
+	  </div>
+	  <!-- Add more cards for other data points as needed -->
+	{:else}
+	  <p>Loading data...</p>
+	{/if}
+  </main>
 	<div class="chart-card">
 	  <h2>Deal Lifecycle Days</h2>
 	  <div id='chart-container-days'></div>
---
+++ src/Card.svelte
@@ -0,0 +1,37 @@
+<!-- Card.svelte -->
+<script>
+    export let title;
+    export let value;
+    export let icon;
+  </script>
+  
+  <div class="card">
+    {#if icon}
+      <div class="icon">
+        <i class="fas {icon}"></i> <!-- Use Font Awesome icon class -->
+      </div>
+    {/if}
+    <div class="title">{title}</div>
+    <div class="value">{value}</div>
+  </div>
+  
+  <style>
+    .card {
+      border: 1px solid #ccc;
+      padding: 10px;
+      text-align: center;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  
+    .icon {
+      font-size: 24px;
+    }
+  
+    .title {
+      font-weight: bold;
+      margin-top: 10px;
+    }
+  </style>
+  
